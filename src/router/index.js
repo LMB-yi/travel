d7b59b75085098180bf36c97eff6ba04b2f36bdd
LMB-yi <2663734627@qq.com>
2026-06-30 23:57:47 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -100,8 +100,12 @@ const routes = [
   },
 ]
 
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes,
+// })
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
